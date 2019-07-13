7a6c619a67d9fb179c2769043adf9a80d286db6c
g4pm0n3y <nate.gappy@gmail.com>
2019-07-13 19:38:37 -0400

    add game functionality added create box functionality added score and reset capability added ability to destroy boxes

--- script.js
+++ script.js
@@ -1,41 +1,96 @@
 $(document).ready(() => {
     
-    // Game Object
-    let gameController = {
-        health: 0,
-        score: 0,
-        level: 0,
-        outcome: null,
-        createBoxes: function() {
-            for(let i = 0; i < 30; i++){
-                let box = {}
-                box.randomX = Math.floor(Math.random() * 11) * 10;
-                box.randomY = Math.floor(Math.random() * 11) * 10;
-                boxes.push(box);
-            }
+// Game Controller Object
+let gameController = {
+    health: 0,
+    score: 0,
+    level: 0,
+    outcome: null,
+    boxes: [],
+    boxesInGame: []
+}
 
-            // append to page on interval
-            setInterval(() => {
-                let randomBox = Math.floor(Math.random() * boxes.length + 1);
-                console.log(boxes[randomBox]);
-                // $('#game')
-                //     .append('<div class="square"></div>')
-                //     .css({'top': })
+// score update function
+gameController.scoreUpdate = () => {
+    gameController.score += 10;
+    $('#score').text(`Score: ${gameController.score}`);
+}
 
-            }, 2000);
+// game reset function
+gameController.gameReset = () => {
+    gameController.health = 0;
+    gameController.score = 0;
+    $('#score').text(`Score: ${gameController.score}`);
+    gameController.boxes = [];
+    $('#game .square').css('display', 'none');
+}
 
-        }
+// // remove box function
+// gameController.boxRemoval = () => {
+
+//     gameController.boxesInGame[0]
+// }
+
+// Box Creation Object
+let boxCreator = {}
+
+// create random box array and append boxes to page on interval
+boxCreator.createBoxes = () => {
+    for(let i = 0; i < 500; i++){
+        let box = {}
+        box.randomX = Math.floor(Math.random() * 11) * 10;
+        box.randomY = Math.floor(Math.random() * 11) * 10;
+        box.color = boxCreator.randomColor();
+        box.name = `box${i}`; 
+        gameController.boxes.push(box);
     }
+    // append random box to page on interval
+    setInterval(() => {
+        let boxes = gameController.boxes;
+        // get random box
+        let randomBox = Math.floor(Math.random() * boxes.length + 1);
+        // append box to page
+        $('#game').append(
+            `<div class="square" id="${boxes[randomBox].name}"></div>`
+        );
+        // customize box based on boxes properties
+        $(`#game #${boxes[randomBox].name}`).css({
+            'top': boxes[randomBox].randomX + '%', 
+            'left': boxes[randomBox].randomY + '%',
+            'background': boxes[randomBox].color
+        })
+        // place boxes to in game box array
+        gameController.boxesInGame.push(boxes[randomBox]);
+    }, 1000);
+}
 
-    // Box Array
-    let boxes = [
+// random box color function
+boxCreator.randomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-    ]
-
-    // Start New Game
-    $('#newgame').on('click', () => {
-        gameController.createBoxes();
-    });
-
-
+// box destroy function
+$('#game').on('click', '.square', (e) => {
+    // hide square on click
+    let box = document.getElementById(e.target.id);
+    box.style.display = 'none';
+    // update player score
+    gameController.scoreUpdate();
 });
+
+
+// start new game
+$('#newgame').on('click', () => {
+    gameController.gameReset();
+    setTimeout(() => {
+        boxCreator.createBoxes();
+    }, 1000)
+});
+
+
+}); // emd of document ready
